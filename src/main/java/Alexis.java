af36da69f1c6d048289c6c2d26e05818377fd9d9
Oh Zhan Jie Alexander <watterson332@gmail.com>
2025-01-24 13:05:33 +0800

    level 2

--- src/main/java/Alexis.java
+++ src/main/java/Alexis.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Alexis {
     public static void main(String[] args) {
+        // create list
+        List<String> words = new ArrayList<>();
+
         // Create a Scanner object to read input from the command line
         Scanner scanner = new Scanner(System.in);
 
@@ -15,14 +20,28 @@ public class Alexis {
             // Prompt the user for input
             String input = scanner.nextLine();
 
+            // display list condition
+            if ("list".equalsIgnoreCase(input)) {
+                if (words.size() > 0) {
+                    int counter = 1;
+                    System.out.println(line);
+                    for (String word : words) {
+                        System.out.println(counter + ". " + word);
+                        counter++;
+                    }
+                    System.out.println(line);
+                }
+            } else {
+                // Echo the input back to the user
+                words.add(input);
+                System.out.println(line + "added: " + input + "\n" + line);
+            }
+
             // Exit condition
             if ("bye".equalsIgnoreCase(input)) {
                 System.out.println(line+ "Bye. Hope to see you again soon!\n" + line);
                 break;
             }
-
-            // Echo the input back to the user
-            System.out.println(line + input + "\n" + line);
         }
 
         // Close the scanner

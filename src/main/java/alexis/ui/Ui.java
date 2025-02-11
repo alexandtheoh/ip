5815aad00556a02c19ab34aff1a4a562e6f7f4dc
Oh Zhan Jie Alexander <watterson332@gmail.com>
2025-02-11 15:08:08 +0800

    Add A-CodeQuality

--- src/main/java/alexis/ui/Ui.java
+++ src/main/java/alexis/ui/Ui.java
@@ -55,6 +55,10 @@ public class Ui {
      * @return output string
      */
     public String recieveInput(String input) {
+        // mark/unmark condition
+        String[] words = input.split(" ");
+        boolean isSpecificCase = words.length == 2 && isInteger(words[1]);
+
         // Exit condition
         if (ActionsEnum.bye.name().equalsIgnoreCase(input)) {
             storage.save();
@@ -63,85 +67,76 @@ public class Ui {
         } else if (ActionsEnum.list.name().equalsIgnoreCase(input)) {
             return line + "Here are the tasks in your list:\n"
                     + this.tasksList.toString() + line;
-        } else {
-            // mark/unmark condition
-            String[] words = input.split(" ");
-
-            if (ActionsEnum.search.name().equalsIgnoreCase(words[0])) { // search
+        } else if (ActionsEnum.search.name().equalsIgnoreCase(words[0])) { // search
                 String searchString = String.join(" ", Arrays.copyOfRange(words, 1, words.length));
                 return line + "Here are the matching tasks in your list:\n"
                         + this.tasksList.search(searchString) + line;
-            } else if (words.length == 2 && isInteger(words[1])) { // case of mark/unmark/delete
-                if (ActionsEnum.mark.name().equalsIgnoreCase(words[0])) {
-                    int pos = Integer.parseInt(words[1]);
-                    try {
-                        Task task = tasksList.markTask(pos);
-                        return line + "Nice! I've marked this task as done:\n"
-                                + task.getStatusIcon() + " " + task.getDescription() + "\n" + line;
-                    } catch (InvalidPosException e) {
-                        return Ui.invalid;
-                    }
-                } else if (ActionsEnum.unmark.name().equalsIgnoreCase(words[0])) {
-                    int pos = Integer.parseInt(words[1]);
-                    try {
-                        Task task = tasksList.unmarkTask(pos);
-                        return line + "OK, I've marked this task as not done yet:\n"
-                                + task.getStatusIcon() + " " + task.getDescription() + "\n" + line;
-                    } catch (InvalidPosException e) {
-                        return Ui.invalid;
-                    }
-                } else if (ActionsEnum.delete.name().equalsIgnoreCase(words[0])) {
-                    int pos = Integer.parseInt(words[1]);
-                    try {
-                        Task task = tasksList.deleteTask(pos);
-                        return line + "Noted. I've removed this task: \n"
-                                + task.toString() + "\nNow you have " + this.tasksList.getSize()
-                                + " tasks in the list.\n" + line;
-                    } catch (InvalidPosException e) {
-                        return Ui.invalid;
-                    }
-                } else {
-                    return Ui.invalid;
-                }
-            } else { // add task
-                String[] taskString = input.split("/");
-                String[] desArr = taskString[0].split(" ");
-                String taskType = desArr[0];
-                String description = String.join(" ", Arrays.copyOfRange(desArr, 1, desArr.length));
-
-                Task task = new Task("0", "dummy");
-
-                if (TaskTypeEnum.todo.name().equalsIgnoreCase(taskType)) {
-                    try {
-                        task = new Todo("0", description);
-                        this.tasksList.addTask(task);
-                    } catch (Exception e) {
-                        return line + e.getMessage() + "\n" + line;
-                    }
-                } else if (TaskTypeEnum.deadline.name().equalsIgnoreCase(taskType)) {
-                    String[] byArr = taskString[1].split(" ");
-                    String by = String.join(" ", Arrays.copyOfRange(byArr, 1, byArr.length));
-
-                    task = new Deadline("0", description, by);
-                    this.tasksList.addTask(task);
-                } else if (TaskTypeEnum.event.name().equalsIgnoreCase(taskType)) {
-                    String[] fromArr = taskString[1].split(" ");
-                    String from = String.join(" ", Arrays.copyOfRange(fromArr, 1, fromArr.length));
-
-                    String[] toArr = taskString[2].split(" ");
-                    String to = String.join(" ", Arrays.copyOfRange(toArr, 1, toArr.length));
-
-                    task = new Event("0", description, from, to);
-                    this.tasksList.addTask(task);
-                } else {
-                    return Ui.invalid;
-                }
-
-                // print task added message
-                return line + "Got it. I've added this task: \n"
-                        + task + "\nNow you have " + this.tasksList.getSize()
-                        + " tasks in the list.\n" + line;
+        } else if (isSpecificCase && ActionsEnum.mark.name().equalsIgnoreCase(words[0])) { // mark case
+            int pos = Integer.parseInt(words[1]);
+            try {
+                Task task = tasksList.markTask(pos);
+                return line + "Nice! I've marked this task as done:\n"
+                        + task.getStatusIcon() + " " + task.getDescription() + "\n" + line;
+            } catch (InvalidPosException e) {
+                return Ui.invalid;
             }
+        } else if (isSpecificCase && ActionsEnum.unmark.name().equalsIgnoreCase(words[0])) { // unmark case
+            int pos = Integer.parseInt(words[1]);
+            try {
+                Task task = tasksList.unmarkTask(pos);
+                return line + "OK, I've marked this task as not done yet:\n"
+                        + task.getStatusIcon() + " " + task.getDescription() + "\n" + line;
+            } catch (InvalidPosException e) {
+                return Ui.invalid;
+            }
+        } else if (isSpecificCase && ActionsEnum.delete.name().equalsIgnoreCase(words[0])) { // delete case
+            int pos = Integer.parseInt(words[1]);
+            try {
+                Task task = tasksList.deleteTask(pos);
+                return line + "Noted. I've removed this task: \n"
+                        + task.toString() + "\nNow you have " + this.tasksList.getSize()
+                        + " tasks in the list.\n" + line;
+            } catch (InvalidPosException e) {
+                return Ui.invalid;
+            }
+        } else { // add task
+            String[] taskString = input.split("/");
+            String[] desArr = taskString[0].split(" ");
+            String taskType = desArr[0];
+            String description = String.join(" ", Arrays.copyOfRange(desArr, 1, desArr.length));
+
+            Task task = new Task("0", "dummy");
+
+            if (TaskTypeEnum.todo.name().equalsIgnoreCase(taskType)) {
+                try {
+                    task = new Todo("0", description);
+                    this.tasksList.addTask(task);
+                } catch (Exception e) {
+                    return line + e.getMessage() + "\n" + line;
+                }
+            } else if (TaskTypeEnum.deadline.name().equalsIgnoreCase(taskType)) {
+                String[] byArr = taskString[1].split(" ");
+                String by = String.join(" ", Arrays.copyOfRange(byArr, 1, byArr.length));
+
+                task = new Deadline("0", description, by);
+                this.tasksList.addTask(task);
+            } else if (TaskTypeEnum.event.name().equalsIgnoreCase(taskType)) {
+                String[] fromArr = taskString[1].split(" ");
+                String from = String.join(" ", Arrays.copyOfRange(fromArr, 1, fromArr.length));
+
+                String[] toArr = taskString[2].split(" ");
+                String to = String.join(" ", Arrays.copyOfRange(toArr, 1, toArr.length));
+
+                task = new Event("0", description, from, to);
+                this.tasksList.addTask(task);
+            } else {
+                return Ui.invalid;
+            }
+
+            // print task added message
+            return line + "Got it. I've added this task: \n"
+                    + task + "\nNow you have " + this.tasksList.getSize()
+                    + " tasks in the list.\n" + line;
         }
     }
 }
